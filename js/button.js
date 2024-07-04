bda58119d61ab25837dd2b5f4ed85517a5f7f0ad
Minel03 <saint.mynel@gmail.com>
2024-07-04 15:26:31 +0800

    final

---
+++ js/button.js
@@ -0,0 +1,19 @@
+function goBack() {
+    window.location.href = "admin-dashboard.php";
+  }
+
+function goReset() {
+    window.location.href = "1st-year-enrollment.php";
+  }
+  function goReset1() {
+    window.location.href = "2nd-year-enrollment.php";
+  }
+  function goReset2() {
+    window.location.href = "3rd-year-enrollment.php";
+  }
+  function goReset3() {
+    window.location.href = "4th-year-enrollment.php";
+  }
+  function goReset4() {
+    window.location.href = "transferee-enrollment.php";
+  }
